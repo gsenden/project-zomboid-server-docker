f2e5859fe95e198cdfc2e64dfb958a6c33140995
Developer <dev@example.com>
2025-06-26 00:19:16 +0200

    Basic layout done

--- server-config-app/src/routes/+page.svelte
+++ server-config-app/src/routes/+page.svelte
@@ -3,13 +3,29 @@
 </script>
 
 <div class="flex items-center justify-center h-screen w-full">
-  <div class="w-full h-full max-w-[1920px] max-h-[1024px] bg-[url('/background.png')] bg-center bg-no-repeat bg-cover p-10">
-    <div class="h-full border-neutral-600 border-1"></div>
+  <div class="w-full h-full max-w-[2304px] max-h-[1229px] bg-[url('/background.png')] bg-center bg-no-repeat bg-cover p-10">
+    <div class="h-full border-neutral-700 border-1 bg-black/30 flex flex-col">
+        <div class="flex justify-center p-1">
+            <h1 class="text-white text-2xl md:text-3xl courier-prime-regular">Edit Server Settings</h1> 
+        </div>
+        <div class="flex-grow flex flex-row">
+            <div class="w-[400px] border-1 border-neutral-700 ml-5 bg-black/30">
+                
+            </div>
+            
+            <div class="flex-1 border-1 border-neutral-700 ml-5 mr-5 bg-black/30">
+                
+            </div>
+        </div>
+        
+        <div class="border-1 border-blue-500 m-5 mb-2 h-10 text-white">footer</div>
+    </div>
   </div>
 </div>
 
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 	/* Global styles that can't be done with Tailwind utilities */
 	:global(body) {
 		margin: 0;
@@ -26,5 +42,11 @@
 
     :global(.content) {
         border: 1px solid red;
+    }
+
+    .courier-prime-regular {
+        font-family: 'Courier Prime', monospace;
+        font-weight: 400;
+        letter-spacing: 0.05em;
     }
 </style>
