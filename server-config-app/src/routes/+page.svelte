b3d3cc0a6b6c52813e44adb67ed9b68dae443edb
Developer <dev@example.com>
2025-06-26 00:53:32 +0200

    Added menu items

--- server-config-app/src/routes/+page.svelte
+++ server-config-app/src/routes/+page.svelte
@@ -9,7 +9,57 @@
             <h1 class="text-white text-2xl md:text-3xl courier-prime-regular">Edit Server Settings</h1> 
         </div>
         <div class="flex-grow flex flex-row">
-            <div class="w-[400px] border-1 border-neutral-700 ml-5 bg-black/30">
+            <div class="w-[400px] border-1 border-neutral-700 ml-5 bg-black/30 text-white">
+                <div class="h-7 bg-neutral-100/20">
+                    <div class="sidebar-item flex items-center pl-2 h-7 bg-neutral-100/20">
+                        <span class="triangle-down mr-2">&#9660;</span>
+                        <span>INI</span>
+                    </div>
+                    
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Details</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Steam</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Backups</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Steam Workshop</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Mods</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Map</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Spawn Regions</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Players</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Admin</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Fire</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">PVP</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Loot</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Faction</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Safehouse</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Chat</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">RCON</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Discord</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">UPnP</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Other</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Vehicles</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Voice</div>
+
+
+                    <div class="sidebar-item flex items-center pl-2 h-7 bg-neutral-100/20">
+                        <span class="triangle-down mr-2">&#9660;</span>
+                        <span>Sandbox</span>
+                    </div>
+                    
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Presets</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Population</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Time</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">World</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Nature</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Sadistic AI Director</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Meta</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Loot Rarity</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Character</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">In-game Map</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Vehicles</div>
+                    <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Zombie Lore</div>
+                       <div class="pl-10 h-7 border-1 border-neutral-100/20 hover:bg-neutral-100/10 cursor-pointer">Advanced Zombie Options</div>
+                
+        
+                </div>
                 
             </div>
             
@@ -26,7 +76,8 @@
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-	/* Global styles that can't be done with Tailwind utilities */
+	
+    /* Global styles that can't be done with Tailwind utilities */
 	:global(body) {
 		margin: 0;
 		padding: 0;
@@ -49,4 +100,5 @@
         font-weight: 400;
         letter-spacing: 0.05em;
     }
+
 </style>
