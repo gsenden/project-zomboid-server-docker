435bef2b7b9275bed3c3ac203c9c4ff5df7e9d79
Developer <dev@example.com>
2025-06-26 20:23:35 +0200

    Initial Mods

---
+++ server-config-app/src/lib/components/Mods.svelte.tests.ts
@@ -0,0 +1,19 @@
+import { page } from '@vitest/browser/context';
+import { describe, expect, it } from 'vitest';
+import { render } from 'vitest-browser-svelte';
+import Mods from './Mods.svelte';
+
+describe('Feature: Mods Configuration', () => {
+	describe('Scenario: User views the Mods section', () => {
+        // it('They should see a way to configure the UDPPort', async () => {
+		// 	// Given: A user wants to configure their Project Zomboid Steam details
+		// 	// When: They navigate to the Steam configuration section
+		// 	render(Steam);
+
+		// 	// Then: They should see a way to configure the UDPPort
+		// 	const udpPortInput = page.getByTestId('UDPPort');
+		// 	await expect.element(udpPortInput).toBeInTheDocument();
+		// });
+
+    });
+});
