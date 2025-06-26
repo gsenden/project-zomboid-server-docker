6ad198805f264ade4a73b8848986ee4fba0e9c64
Developer <dev@example.com>
2025-06-26 20:25:47 +0200

    Inital Map

---
+++ server-config-app/src/lib/components/Map.svelte.tests.ts
@@ -0,0 +1,19 @@
+import { page } from '@vitest/browser/context';
+import { describe, expect, it } from 'vitest';
+import { render } from 'vitest-browser-svelte';
+import Map	from './Map.svelte';
+
+describe('Feature: Map Configuration', () => {
+	describe('Scenario: User views the Map section', () => {
+        // it('They should see a way to configure the UDPPort', async () => {
+		// 	// Given: A user wants to configure their Project Zomboid Steam details
+		// 	// When: They navigate to the Steam configuration section
+		// 	render(Steam);
+
+		// 	// Then: They should see a way to configure the UDPPort
+		// 	const udpPortInput = page.getByTestId('UDPPort');
+		// 	await expect.element(udpPortInput).toBeInTheDocument();
+		// });
+
+    });
+});
